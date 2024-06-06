6bf2dff9fb6b126241339fe0a48e7b8aa18f77f0
Matthew Deloughry <matt@deloughry.co.uk>
2024-06-06 07:18:41 +0100

    added Feed managment

--- routes/console.php
+++ routes/console.php
@@ -4,4 +4,4 @@ use App\Jobs\FetchArticles;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::job(new FetchArticles)->hourly();
-Schedule::command('email:daily-summary')->dailyAt('07:00');
+Schedule::command('email:daily-summary')->dailyAt('07:00')->timezone('Europe/London');
