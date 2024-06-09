d447dd8ecec05f5587efd517827d47bdfaddfa06
Matthew Deloughry <matt@deloughry.co.uk>
2024-06-09 14:11:47 +0100

    Hopefully sped up processing articles

--- app/Actions/FeedTools.php
+++ app/Actions/FeedTools.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions;
 
+use App\Models\Article;
 use App\Models\Feed;
 use DOMDocument;
 use Illuminate\Support\Collection;
@@ -11,23 +12,24 @@ use League\HTMLToMarkdown\HtmlConverter;
 use Masterminds\HTML5;
 use SimplePie\Item;
 use SimplePie\SimplePie;
-use Str;
 
-class FeedTools {
-     // Function to check if the URL is a valid RSS feed
+class FeedTools
+{
+    // Function to check if the URL is a valid RSS feed
     public static function isValidFeed($url)
     {
         $response = Http::get($url);
         if ($response->successful()) {
             $contentType = $response->header('Content-Type');
+
             return strpos($contentType, 'application/rss+xml') !== false ||
                 strpos($contentType, 'application/atom+xml') !== false ||
                 strpos($contentType, 'text/xml') !== false ||
                 strpos($contentType, 'application/xml') !== false;
         }
+
         return false;
     }
-
 
     public static function findValidFeedUrl($url)
     {
@@ -39,7 +41,7 @@ class FeedTools {
 
         // Fetch the HTML content of the given URL
         $response = Http::get($url);
-        if (!$response->successful()) {
+        if (! $response->successful()) {
             return null;
         }
 
@@ -66,21 +68,23 @@ class FeedTools {
         return null;
     }
 
-    public static function fetchFavicon($url) {
+    public static function fetchFavicon($url)
+    {
         //take the url and get the root domain
         $url = parse_url($url, PHP_URL_HOST);
         $url = explode('.', $url);
-        $url = $url[count($url) - 2] . '.' . $url[count($url) - 1];
-        $url = "https://www.google.com/s2/favicons?domain=" . $url;
+        $url = $url[count($url) - 2].'.'.$url[count($url) - 1];
+        $url = 'https://www.google.com/s2/favicons?domain='.$url;
+
         return $url;
     }
 
-    private static function fetchFeed($url) : SimplePie
+    private static function fetchFeed($url): SimplePie
     {
         $feed = new SimplePie();
 
         $response = Http::get($url);
-        if (!$response->successful()) {
+        if (! $response->successful()) {
             throw new \Exception("Failed to fetch feed: {$url}");
         }
 
@@ -88,17 +92,17 @@ class FeedTools {
         $feed->init();
         $feed->handle_content_type();
 
-        if ($feed->error())
-        {
+        if ($feed->error()) {
             throw new \Exception($feed->error());
         }
 
         return $feed;
     }
 
-    public static function getFeedTitle($url) : string
+    public static function getFeedTitle($url): string
     {
         $feed = self::fetchFeed($url);
+
         return $feed->get_title();
     }
 
@@ -118,20 +122,20 @@ class FeedTools {
                 'category' => $item->get_category()?->term ?? null,
             ];
         }
+
         return $items;
     }
 
     public static function fetchContent(Item $item)
     {
         $data = null;
-        if ($item->get_content(content_only: true) )
-        {
+        if ($item->get_content(content_only: true)) {
             $data = $item->get_content(content_only: true);
         }
         //if $item->data['child'] has key "https://purl.org/rss/1.0/modules/content/" get ['encoded'][0]['data]
         if (isset($item->data['child']['https://purl.org/rss/1.0/modules/content/'])) {
             $data = $item->data['child']['https://purl.org/rss/1.0/modules/content/']['encoded'][0]['data'];
-            Log::info("Found content in RSS feed");
+            Log::info('Found content in RSS feed');
             $html5Parser = new HTML5();
             $dom = $html5Parser->loadHTML($data);
             $data = $dom->saveHTML();
@@ -139,9 +143,10 @@ class FeedTools {
             // try and get the Article content from the HTML, by fetching $item->get_permalink()
             // and using DOMDocument to parse the HTML and find article content
             $response = Http::get($item->get_permalink());
-            if (!$response->successful()) {
-                Log::alert("Unable to fetch Articles");
-                return "";
+            if (! $response->successful()) {
+                Log::alert('Unable to fetch Articles');
+
+                return '';
             }
             try {
                 $html = $response->body();
@@ -150,67 +155,70 @@ class FeedTools {
 
                 $article = $dom->getElementsByTagName('article');
                 if ($article) {
-                    if ( $article->count() > 0 && $article->item(0)->nodeValue) {
+                    if ($article->count() > 0 && $article->item(0)->nodeValue) {
                         $data = $article->item(0)->nodeValue;
                     }
                 }
-                Log::info("Found article in HTML");
+                Log::info('Found article in HTML');
             } catch (\Exception $e) {
                 return '';
             }
         }
 
-
         if ($data) {
             $convertor = new HtmlConverter();
-            Log::info("Converting content");
+            Log::info('Converting content');
             $data = $convertor->convert($data);
             //remove <html xmlns="http://www.w3.org/1999/xhtml"><body> from $data
             $data = str_replace('<html xmlns="http://www.w3.org/1999/xhtml"><body>', '', $data);
+
             return $data;
             // return $data;
         }
-        Log::info("No content found");
+        Log::info('No content found');
+
         return '';
 
     }
-
 
     public static function processArticles($feeds = null)
     {
         if ($feeds === null) {
             $feeds = Feed::where('last_fetched_at', '<', now()->subHours(1))
-            ->orWhereNull('last_fetched_at')->get();
+                ->orWhereNull('last_fetched_at')->get();
         }
 
         // if $feeds is UUID, Fetch Feed and convert to collection
         if ($feeds instanceof Feed) {
             $feeds = collect([$feeds]);
-        } elseif (!$feeds instanceof Collection) {
+        } elseif (! $feeds instanceof Collection) {
             return;
         }
 
         foreach ($feeds as $feed) {
             $feedItems = FeedTools::fetchFeedItems($feed->url);
 
-            foreach ($feedItems as $feedItem) {
-                /** @var \App\Models\Feed $feedItem */
-                $feed->articles()->updateOrCreate([
-                    'link' => $feedItem['link'],
-                    'feed_id' => $feed->id,
-                ],
-                [
+            //get articles that are not already in the database
+            $newArticles = collect($feedItems)->filter(function ($article) use ($feed) {
+                return ! Article::where('link', '=', $article['link'])
+                    ->where('feed_id', '=', $feed->id)->exists();
+            });
+            // iterate over each article and make a new entry, ready to bulk insert
+            $articles = collect([]);
+            foreach ($newArticles as $feedItem) {
+                $articles->push($feed->articles()->make([
                     'title' => $feedItem['title'],
                     'content' => $feedItem['content'],
                     'description' => $feedItem['description'],
                     'category' => $feedItem['category'],
                     'author' => $feedItem['author'],
-                    'published_at' => $feedItem['published_at']
-                ]
-                );
+                    'published_at' => $feedItem['published_at'],
+                    'link' => $feedItem['link'],
+                ]));
             }
+            // bulk insert the new articles
+            $feed->articles()->saveMany($articles);
             $feed->update(['last_fetched_at' => now()]);
         }
     }
 }
-
